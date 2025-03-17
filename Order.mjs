bb27268ae144baa5a208e407b4155183d4775814
albesanna <albertosanna02@gmail.com>
2025-03-17 17:07:30 +0100

    Nuove funzioni in Order.mjs

--- Order.mjs
+++ Order.mjs
@@ -1,10 +1,22 @@
-function Order(id_order) {
+import { Poke } from './Poke.mjs';
+
+function Order(id_order, poke, quantity) {
     this.id_order = id_order;
     this.pokes = [];
-
-    this.add = function(id_poke, quantity) {
-        this.pokes.push([id_poke, quantity]);
+    if(poke instanceof Poke) {
+        this.pokes.push([poke, quantity]);
+    } else {
+        throw new Error('Non stai inserendo un oggetto di tipo Poke');
     }
+
+    this.add = function(poke, quantity) {
+        if (poke instanceof Poke) {
+            this.pokes.push([poke, quantity]);
+        } else {
+            throw new Error('Non stai inserendo un oggetto di tipo Poke');
+        }
+    }
+    
 
     this.mod_quantity = function(id_poke, quantity) {
         this.pokes = this.pokes.map(order => {
@@ -18,7 +30,23 @@ function Order(id_order) {
     this.remove = function(id_poke) {
         this.pokes = this.pokes.filter(order => order[0] !== id_poke);
     }
+
+    this.getTotalQuantity = function() {
+        let totalQuantity = 0;
+        totalQuantity = this.pokes.reduce((acc, order) => acc + order[1], 0);
+        return totalQuantity;
+    }
+
+    this.getPrice = function() {
+        let totalPrice = 0;
+        totalPrice = this.pokes.reduce((acc, order) => acc + order[0].price_poke * order[1], 0);
+        if (this.pokes.getTotalQuantity() > 4) {
+            totalPrice = totalPrice * 0.9;
+        }
+        return totalPrice;
+    }
 }
+
 
 //esempio
 
