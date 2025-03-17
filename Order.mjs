8161a7e63026af7f0c0478ae49d2a6e9b819a3e8
albesanna <albertosanna02@gmail.com>
2025-03-17 15:13:58 +0100

    Modifica Order.mjs

--- Order.mjs
+++ Order.mjs
@@ -1,18 +1,36 @@
-function Order(id_order, orders) {
+function Order(id_order) {
     this.id_order = id_order;
-    this.orders = [];
+    this.pokes = [];
 
-    this.add = function(poke, quantity) {
-        this.orders.push([poke, quantity]);
+    this.add = function(id_poke, quantity) {
+        this.pokes.push([id_poke, quantity]);
     }
 
-    this.remove = function(poke) {
-        this.orders = this.orders.filter(order => order[0] !== poke);
+    this.mod_quantity = function(id_poke, quantity) {
+        this.pokes = this.pokes.map(order => {
+            if(order[0] === id_poke) {
+                order[1] = quantity;
+            }
+            return order;
+        });
+    }
+
+    this.remove = function(id_poke) {
+        this.pokes = this.pokes.filter(order => order[0] !== id_poke);
     }
 }
 
-const order = new Order(3);
+//esempio
 
-console.log(order.orders);
+const order = new Order(3);
+order.add('poke1', 2);
+order.add('poke2', 3);
+console.log(order.id_order, order.pokes);
+
+order.remove('poke1');
+console.log(order.id_order, order.pokes);
+
+order.mod_quantity('poke2', 5);
+console.log(order.id_order, order.pokes);
 
 
